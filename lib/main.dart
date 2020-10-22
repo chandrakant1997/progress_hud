1c908b6f0af443134a42d7ba9ffda592a467e72e
chandrakant1997 <ckpatel802@gmail.com>
2020-10-22 20:26:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:progress_hud/progress.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'flutter application',
+      home: MyHomePage(),
+    ),
+  );
+}
